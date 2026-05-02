dad9709ee93084bfd6a3167ef309329d91781a72
smalkaisk-hash <smalkaisk@gmail.com>
2026-05-02 16:33:12 +0300

    Add new Python scripts

---
+++ Academy_Award_for_Best_Picture.py
@@ -0,0 +1,11 @@
+
+best_pictures = []
+
+best_pictures.append('2025 - One Battle After Another')
+best_pictures.append('2024 - Anora')
+best_pictures.append('2023 - Oppenheimer')
+best_pictures.append('2022 - Everything Everywhere All at Once')
+best_pictures.append('2021 - CODA')
+
+for item in best_pictures:
+    print(item)
---
+++ wishlist.py
@@ -0,0 +1,11 @@
+
+
+wishlist = ['Apple Macbook Pro',
+ 'Nas server',
+ 'LLM server',
+ 'Get permanent job after internship']
+
+
+
+for item in wishlist:
+    print(item)
